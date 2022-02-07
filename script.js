7199bb177e543acf923b060aa6ce723c8f9f3980
thomas <hello@thomasflorentin.net>
2022-02-07 16:33:52 +0100

    Polish

--- script.js
+++ script.js
@@ -6,42 +6,42 @@ document.addEventListener('DOMContentLoaded', () => {
 
     var slider_large = tns({
         container: '.slide_large',
-        items: 1.2,
+        items: 1.4,
         gutter: 32,
         autoplay: false,
         loop: false,
         responsive: {
-            1000: {
-              items: 2.2
-            }
+          700: {
+            items: 2.2
           }
+        }
     });
 
 
     var slider_medium = tns({
         container: '.slide_medium',
-        items: 1.2,
+        items: 1.4,
         gutter: 32,
         autoplay: false,
         loop: false,
         responsive: {
-            1000: {
-              items: 3.2
-            }
+          700: {
+            items: 3.2
           }
+        }
     });
 
     var slider_small = tns({
       container: '.slide_small',
-      items: 1.2,
+      items: 1.4,
       gutter: 32,
       autoplay: false,
       loop: false,
       responsive: {
-          1000: {
-            items: 4
-          }
+        700: {
+           items: 4
         }
+      }
   });
 
 })
